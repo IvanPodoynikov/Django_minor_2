c9ceea70e6f491f30641a6c45ae0fc405d092ce1
IvanPodoynikov <ipodoynikov@yandex.ru>
2025-05-20 00:44:29 +0300

    добавляем работающую админку

--- eduapp/admin.py
+++ eduapp/admin.py
@@ -1,3 +1,43 @@
+# eduapp/admin.py
 from django.contrib import admin
+from .models import Entry
 
-# Register your models here.
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    # поля, которые увидите в таблице
+    list_display = (
+        'id',
+        'last_name',
+        'first_name',
+        'program',
+        'email',
+        'phone',
+        'score',
+        'date',
+    )
+
+    # фильтры справа: по программе, по баллу, по дате
+    list_filter = (
+        'program',
+        'score',
+        'date',
+    )
+
+    # строка поиска по фамилии, имени и e-mail
+    search_fields = (
+        'last_name',
+        'first_name',
+        'email',
+    )
+
+    # по умолчанию сортировать по дате (от поздних к старым)
+    ordering = ('-date',)
+
+    # править прямо в списке можно будет балл и телефон
+    list_editable = (
+        'score',
+        'phone',
+    )
+
+    # опционально: максимально сколько записей одновременно
+    list_per_page = 25
